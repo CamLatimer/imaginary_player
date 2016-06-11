2ab22ac7ffeddd308976aca7e80357c1de50a2ea
Cam <camlatimer@gmail.com>
2016-06-11 14:32:51 -0400

    search controller uses input from search box to make a search

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,13 +5,19 @@
   angular
   .module('controllers', [])
   .controller('SearchCtrl', function($scope, $http){
-    var input = 'anna lunoe';
-    $http.get('https://api.soundcloud.com/tracks?q=' + input + '&limit=200&filter=public&client_id=4f2b9615c8783056a2eae41eba103c48')
-    .then(function(response){
-      $scope.tracks = response
-    });
-    $scope.showSongs = function(){
-      console.log($scope.tracks);
+    // grab input from search box
+    $scope.input;
+    $scope.getSongs = function(){
+      var input = $scope.input;
+      input = input.split(' ').join('+');
+      var url = 'https://api.soundcloud.com/tracks?q=' + input + '&limit=200&filter=public&client_id=4f2b9615c8783056a2eae41eba103c48';
+      $http.get(url)
+      .then(function(response){
+        $scope.tracks = response
+        console.log($scope.tracks);
+        console.log($scope.input);
+        console.log(url);
+      });
     }
 
   })
