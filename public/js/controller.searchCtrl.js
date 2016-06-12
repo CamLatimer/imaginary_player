2c83db55d7aa3013558b211815f296f4012f804f
Cam <camlatimer@gmail.com>
2016-06-12 10:58:31 -0400

    makes sure load more button is only shown when there are more songs to show after a search call is made

--- public/js/controller.searchCtrl.js
+++ public/js/controller.searchCtrl.js
@@ -5,12 +5,12 @@
   angular
   .module('search', [])
   .controller('SearchCtrl', function($scope, $http){
-    // grab input from search box
+
     $scope.input;
     $scope.tracks = [];
     $scope.nextLink;
-    var url;
 
+    // grab input from search box
     $scope.getInput = function(){
       $scope.tracks = [];
       var input = $scope.input;
@@ -18,6 +18,7 @@
       var inputUrl = 'https://api.soundcloud.com/search?q=' + input + '&limit=20&linked_partitioning&client_id=4f2b9615c8783056a2eae41eba103c48';
       return inputUrl;
     }
+    // makes call to api
     $scope.loadSongs = function(url){
       return $http.get(url)
       .then(function(response){
@@ -25,19 +26,27 @@
         $scope.collection.forEach(function(track){
           $scope.tracks.push(track);
         });
-        $scope.nextLink = response.data.next_href;
+        // if there is a url to get more data, show the load more button
+        if(response.data.next_href){
+          $scope.nextLink = response.data.next_href;
+          $scope.showBtn = true;
+        } else {
+          $scope.showBtn = false;
+        }
+        // just to peep what comes back from the api call
         console.dir(response);
-        console.log($scope.nextLink);
         });
     }
-
+    // runs the search with user input and the AJAX
     $scope.search = function(){
       var myInput = $scope.getInput();
       $scope.loadSongs(myInput);
     }
+    // triggers api call to get next round of data
     $scope.loadMore = function(){
       $scope.loadSongs($scope.nextLink);
     }
+
   })
 })();
 
